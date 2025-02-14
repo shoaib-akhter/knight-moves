3bb4d601d3cb5e549d8caed66500d9b4ed718687
Shoaib Akhtar <56090706+shoaib-akhter@users.noreply.github.com>
2025-02-15 01:20:51 +0500

    Refactor knight_moves.rb for Rubocop compliance

--- lib/knight_moves.rb
+++ lib/knight_moves.rb
@@ -1,55 +1,54 @@
-def valid_moves(position)
-    x, y = position
-    moves = [
-        [x + 2, y + 1], [ x + 2, y -1],
-        [x - 2, y + 1], [x - 2, y - 1],
-        [x + 1, y + 2], [x + 1, y - 2],
-        [x - 1, y + 2], [x - 1, y - 2]
-    ]
+# frozen_string_literal: true
 
-    # Keep only moves within the board boundaries
-    moves.select { |new_x, new_y| new_x.between?(0, 7) && new_y.between?(0, 7)}
+def generate_knight_moves(position)
+  x, y = position
+  [
+    [x + 2, y + 1], [x + 2, y - 1], [x - 2, y + 1], [x - 2, y - 1],
+    [x + 1, y + 2], [x + 1, y - 2], [x - 1, y + 2], [x - 1, y - 2]
+  ]
+end
+
+def valid_moves(position)
+  generate_knight_moves(position).select { |x, y| x.between?(0, 7) && y.between?(0, 7) }
 end
 
 def initialize_bfs(start)
-    queue = [start]
-    parent_tracker = { start => nil }
-    visited = Set.new([start])
-    [queue, parent_tracker, visited]
+  queue = [start]
+  parent_tracker = { start => nil }
+  visited = Set.new([start])
+  [queue, parent_tracker, visited]
 end
 
 def target_reached?(current_position, target)
-    current_position == target
+  current_position == target
 end
 
-def is_valid_move?(position, visited)
-    x, y = position
-    x.between?(0, 7) && y.between?(0, 7) && !visited.include?(position)
+def valid_move?(position, visited)
+  x, y = position
+  x.between?(0, 7) && y.between?(0, 7) && !visited.include?(position)
 end
-  
+
 def explore_moves(current_position, queue, visited, parent_tracker)
-    valid_moves(current_position).each do |move|
-      next unless is_valid_move?(move, visited)
-  
-      visited.add(move)
-      parent_tracker[move] = current_position
-      queue.push(move)
-    end
+  valid_moves(current_position).each do |move|
+    next unless valid_move?(move, visited)
+
+    visited.add(move)
+    parent_tracker[move] = current_position
+    queue.push(move)
+  end
 end
 
 def bfs_search(start, target)
-    return { target => nil } if start == target
-  
-    queue, parent_tracker, visited = initialize_bfs(start)
-  
-    until queue.empty?
-      current_position = queue.shift
-      return parent_tracker if target_reached?(current_position, target)
-  
-      explore_moves(current_position, queue, visited, parent_tracker)
-    end
-  
-    {}
+  return { target => nil } if start == target
+
+  queue, parent_tracker, visited = initialize_bfs(start)
+
+  until queue.empty?
+    current_position = queue.shift
+    return parent_tracker if target_reached?(current_position, target)
+
+    explore_moves(current_position, queue, visited, parent_tracker)
+  end
+
+  {}
 end
-  
-  
